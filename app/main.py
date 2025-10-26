d0387f69e5e55cee6beafa4ae9e3171580bd8c07
Jackpot <brunoruizn@gmail.com>
2025-10-26 20:39:37 -0300

    Api Tittle changed

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from app.core.db import init_db
 from app.modules.auth.router import router as auth_router
 from app.modules.pets.router import router as pets_router
 
-app = FastAPI(title="PetCare API", version="0.1", swagger_ui_parameters={"persistAuthorization": True})
+app = FastAPI(title="Petracker API", version="0.1", swagger_ui_parameters={"persistAuthorization": True})
 
 @app.on_event("startup")
 async def on_startup():
